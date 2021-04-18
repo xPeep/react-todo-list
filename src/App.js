eb5d61fa1d39159efca1a11ef217fab3c614b23a
xPeep <44339484+xPeep@users.noreply.github.com>
2021-04-19 01:43:31 +0200

    create eshop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
 import './App.css';
+import Product from "./product";
+import {useEffect, useState} from "react";
+import ProductForm from "./product-form";
+import {BrowserRouter as Router, Link, Route, Switch} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [cart, setCart] = useState([])
+    const [data, setData] = useState([])
+    const [isPending, setIsPending] = useState(true)
+    const [error, setError] = useState()
+
+    const onNewProductHandler = (product) => {
+        const newData = [...data];
+        newData.push(product);
+        console.log(newData);
+        setData(newData)
+    }
+
+
+    useEffect(() => {
+        setTimeout(() => {
+                fetch('http://localhost:3001/products')
+                    .then(response => {
+                        if (response.ok) {
+                            return response.json()
+                        }
+                        throw new Error("Unable get data: " + response.statusText)
+                    })
+                    .then(json => setData(json))
+                    .catch((err) => setError(err.message))
+                    .finally(() => setIsPending(false))
+            }
+            , 1000)
+    }, [])
+
+    const addToCartHandler = function (product) {
+        const newCart = [...cart];
+        newCart.push(product);
+        console.log(newCart);
+        setCart(newCart)
+    }
+
+    const removeToCartHandler = function (product) {
+        const newCart = [...cart];
+        const productIndex = newCart.findIndex(item => item.id === product.id)
+        newCart.splice(productIndex, 1)
+        setCart(newCart)
+    }
+
+    return (
+        <Router>
+            <div className="App">
+
+                <nav>
+                    <ul>
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/edit-product">Edit product</Link>
+                        </li>
+                        <li>
+                            <Link to="/cart">Cart</Link>
+                        </li>
+                    </ul>
+                </nav>
+
+
+                <Switch>
+                    <Route path="/cart">
+                        <h1>Shopping cart</h1>
+                        {cart.map(item => <div>{item.name}
+                            <button onClick={()=>removeToCartHandler(item)}>-</button>
+                        </div>)}
+                    </Route>
+                    <Route path="/edit-product">
+                        <ProductForm onNewProduct={onNewProductHandler}/>
+                    </Route>
+                    <Route path="/">
+                        {<div>{cart.length}</div>}
+                        {isPending && "Loading data..."}
+                        {error && <div>{error}</div>}
+                        <div style={{
+                            display: "flex",
+                            flexWrap: "wrap",
+                            margin: "5px"
+                        }}>
+                            {data.map(item => <Product key={item.id} product={item}
+                                                       onClickHandler={addToCartHandler}/>)}
+                        </div>
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
